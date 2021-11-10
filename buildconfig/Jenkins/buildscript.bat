5385c4c6667cd4426d2ba9d05e1436b38d4bb9ae
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2021-11-10 11:31:59 +0000

    Allow BUILD_PACKAGE Jenkins option on any job type
    There is no reason to limit the availability of this job parameter to just
    the pull requests

--- buildconfig/Jenkins/buildscript.bat
+++ buildconfig/Jenkins/buildscript.bat
@@ -76,14 +76,13 @@ if not "%JOB_NAME%" == "%JOB_NAME:clean=%" (
   set BUILDPKG=yes
 )
 
-:: BUILD_PACKAGE can be provided as a job parameter on the pull requests
-if not "%JOB_NAME%" == "%JOB_NAME:pull_requests=%" (
-  if not "%BUILD_PACKAGE%" == "%BUILD_PACKAGE:true=%" (
-    set BUILDPKG=yes
-  ) else (
-    set BUILDPKG=no
-  )
+:: BUILD_PACKAGE can be provided as a job parameter
+if not "%BUILD_PACKAGE%" == "%BUILD_PACKAGE:true=%" (
+  set BUILDPKG=yes
+) else (
+  set BUILDPKG=no
 )
+
 :: Never want package for debug builds
 if not "%JOB_NAME%" == "%JOB_NAME:debug=%" (
   set BUILDPKG=no
