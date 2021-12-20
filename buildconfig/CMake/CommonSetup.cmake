3a5fa2c148594af963ff7e746101c12fc9b6b503
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-12-20 09:39:07 +0000

    Remove debug warning messages

--- buildconfig/CMake/CommonSetup.cmake
+++ buildconfig/CMake/CommonSetup.cmake
@@ -138,7 +138,6 @@ set(MtdVersion_WC_LAST_CHANGED_DATETIME 0)
 set(MtdVersion_WC_LAST_CHANGED_SHA Unknown)
 set(MtdVersion_WC_LAST_CHANGED_BRANCHNAME Unknown)
 set(NOT_GIT_REPO "Not")
-message(WARNING "Git found: ${GIT_FOUND}")
 
 if(GIT_FOUND)
   # Get the last revision
@@ -149,7 +148,6 @@ if(GIT_FOUND)
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
-  message(WARNING ${NOT_GIT_REPO})
   if(NOT NOT_GIT_REPO) # i.e This is a git repository! "git describe" was originally used to produce this variable and
                        # this prefixes the short SHA1 with a 'g'. We keep the same format here now that we use rev-parse
     set(MtdVersion_WC_LAST_CHANGED_SHA "g${GIT_SHA_HEAD}")
