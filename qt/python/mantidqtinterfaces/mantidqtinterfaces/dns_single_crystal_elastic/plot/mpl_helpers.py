9361e7113ba6141ef7baad11dbb02f3e4341117f
Oleksandr Koshchii <o.koshchii@fz-juelich.de>
2023-12-22 17:05:18 +0100

    Add advanced plotting capabilities

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/dns_single_crystal_elastic/plot/mpl_helpers.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/dns_single_crystal_elastic/plot/mpl_helpers.py
@@ -6,7 +6,18 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 
 import matplotlib as mpl
+from matplotlib import colors
+
+
+def get_log_norm(log, zlim):
+    if log:
+        return colors.SymLogNorm(linthresh=0.001, vmin=zlim[0], vmax=zlim[1])
+    return colors.Normalize(vmin=zlim[0], vmax=zlim[1])
 
 
 def get_cmap(colormap_name):
     return mpl.colormaps[colormap_name]
+
+
+def get_shading(axis_type):
+    return axis_type["shading"]
